e4d0e3f13e6497f1de95d847049d66af564dcf64
albertss_chen <albertss_chen@asus.com>
2025-05-13 22:11:54 +0800

    add some keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,9 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save file" })    -- Normal 模式
-vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { desc = "Save file" }) -- Insert 模式
+vim.keymap.set("i", "<C-s>", function()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>:w<CR>", true, false, true), "n", false)
+end, { desc = "Save and stay in Normal mode" })
+
+vim.keymap.set("v", "<", "<gv", { desc = "Indent to the right consecutive"}) -- Increase indent
+vim.keymap.set("v", ">", ">gv", { desc = "Indent to the left consecutive"}) --Decrease indent
