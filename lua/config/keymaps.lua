eeb08c48f95e811b0761255eac659909ff938054
albertss_chen <albertss_chen@asus.com>
2025-06-05 20:56:50 +0800

    force saving file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save file" })    -- Normal 模式
+vim.keymap.set("n", "<C-s>", ":w!<CR>", { desc = "Save file" })    -- Normal 模式
 vim.keymap.set("i", "<C-s>", function()
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>:w<CR>", true, false, true), "n", false)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>:w!<CR>", true, false, true), "n", false)
 end, { desc = "Save and stay in Normal mode" })
 
 vim.keymap.set("v", "<", "<gv", { desc = "Indent to the right consecutive"}) -- Increase indent
