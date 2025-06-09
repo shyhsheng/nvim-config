a1b38f2d456faac5261c10277da444cc59b37b44
albertss_chen <albertss_chen@asus.com>
2025-06-09 09:02:02 +0800

    remove ray-x/lsp_signature.nvim

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -65,16 +65,16 @@ return {
         end,
     },
 
-    -- 參數簽名顯示
-    {
-        "ray-x/lsp_signature.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("lsp_signature").setup({
-                bind = true,
-                hint_enable = true,
-                handler_opts = { border = "rounded" },
-            })
-        end,
-    },
+    -- -- 參數簽名顯示
+    -- {
+    --     "ray-x/lsp_signature.nvim",
+    --     event = "VeryLazy",
+    --     config = function()
+    --         require("lsp_signature").setup({
+    --             bind = true,
+    --             hint_enable = true,
+    --             handler_opts = { border = "rounded" },
+    --         })
+    --     end,
+    -- },
 }
